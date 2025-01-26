8264d58be2674fd4e2ede782ecfb49a3b14c2166
Abhi Pandit <abhi2505p@gmail.com>
2025-01-26 11:37:07 +0530

    Add files via upload

---
+++ EC2_Creation/c1-versions.tf
@@ -0,0 +1,18 @@
+#terraform block
+
+terraform {
+  required_version = "~>0.14"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+#Provider block
+
+provider "aws" {
+  profile = "default"
+  region  = "ap-south-1"
+}
